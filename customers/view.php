a0c82144352bfc4873fae2321efff25d01f375f7
Mateus C. <129549807+oc-mateus@users.noreply.github.com>
2024-10-16 19:33:06 -0300

    Update view.php

--- customers/view.php
+++ customers/view.php
@@ -1,68 +1,81 @@
 <?php
-//Esse é o view.php
 require_once('functions.php');
-if (!isset($_SESSION)) session_start();
-if (isset($_SESSION['user'])){ // Verifica se tem um usuário logado
-    if ($_SESSION['user'] != 'admin'){ // Verifica se o usuário é admin
-        $_SESSION['message'] = "Você precisa ser administrador para acessar esse recurso!";
-        $_SESSION['type'] = "danger";
-        header('Location: ' . BASEURL . 'index.php');
-    }
-} else {
-    $_SESSION['message'] = "Você precisa estar logado e ser administrador para acessar esse recurso!";
-    $_SESSION['type'] = "danger";
-    header('Location: ' . BASEURL . 'index.php');
-}
-
 view($_GET['id']);
 include(HEADER_TEMPLATE);
 ?>
 
-<?php if (!empty($_SESSION['message'])) : ?>
-<div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert" id="actions">
-    <php echo $_SESSION['message']; ?>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-<?php endif; ?>
 
-<?php if (empty($_SESSION['message'])) : ?>
-<header>
-    <h2>Cliente <?php echo $usuario['id']; ?></h2>
-</header>
+
+<h2>Cliente <?php echo $customer['id']; ?></h2>
 <hr>
-<dl class="dl-horizontal">
-    <dt>Nome:</dt>
-    <dd><?php echo $usuario['nome']; ?></dd>
 
-    <dt>Login:</dt>
-    <dd><?php echo $usuario['user']; ?></dd>
-
-    <dt>Senha:</dt>
-    <dd><?php echo $usuario['password']; ?></dd>
-
-    <dt>Foto:</dt>
-    <dd>
-        <?php
-        if(!empty($usuario['foto'])) {
-            echo "<img src=\"fotos/" . $usuario['foto'] . "\" class=\"shadow p-1 mb-1 bg-body rounded\" width=\"300px\">";
-        } else {
-            echo "<img src=\"fotos/semimagem.jpg\" class=\"shadow p-1 mb-1 bg-body rounded\" width=\"300px\">";
-        }
-        ?>
-    </dd>
-</dl>
+<?php if (!empty($_SESSION['message'])): ?>
+	<div class="alert alert-<?php echo $_SESSION['type']; ?>"><?php echo $_SESSION['message']; ?></div>
 <?php endif; ?>
 
-<div id="actions" class="row">
-    <div class="col-md-12">
-        <?php if (empty($_SESSION['message'])) : ?>
-        <a href="edit.php?id=<?php echo $usuario['id']; ?>" class="btn btn-secondary">
-            <i class="fa-solid fa-pen-to-square"></i> Editar
-        </a>
-        <?php endif; ?>
-        <a href="index.php" class="btn btn-light"><i class="fa-solid fa-rotate-left"></i> Voltar</a>
-    </div>
+<dl class="dl-horizontal">
+	<dt>Nome / Razão Social:</dt>
+	<dd><?php echo $customer['name']; ?></dd>
+
+	<dt>CPF:</dt>
+	<dd><?php echo formatarCPF($customer['cpf_cnpj']); ?></dd>
+
+	<dt>Data de Nascimento:</dt>
+	<dd><?php echo formatadata($customer['birthdate'], "d/m/Y"); ?></dd>
+</dl>
+
+<dl class="dl-horizontal">
+	<dt>Endereço:</dt>
+	<dd><?php echo $customer['address']; ?></dd>
+
+	<dt>Bairro:</dt>
+	<dd><?php echo $customer['hood']; ?></dd>
+
+	<dt>CEP:</dt>
+	<dd><?php echo cep($customer['zip_code']); ?></dd>
+
+	<dt>Data de Cadastro:</dt>
+	<dd><?php echo formatadata($customer['created'], "d/m/Y - H:i:s"); ?></dd>
+
+	<dt>Data da ultima atualização:</dt>
+	<dd><?php echo formatadata($customer['modified'], "d/m/Y - H:i:s"); ?></dd>
+</dl>
+
+<dl class="dl-horizontal">
+	<dt>Cidade:</dt>
+	<dd><?php echo $customer['city']; ?></dd>
+
+	<dt>Telefone:</dt>
+	<dd><?php echo celPhone($customer['phone']); ?></dd>
+
+	<dt>Celular:</dt>
+	<dd><?php echo telefone($customer['mobile']); ?></dd>
+
+	<dt>UF:</dt>
+	<dd><?php echo $customer['state']; ?></dd>
+
+	<dt>Inscrição Estadual:</dt>
+	<dd><?php echo $customer['ie']; ?></dd>
+</dl>
+
+<dl class="dl-horizontal">
+	<dt>Foto:</dt>
+	<dd>
+      <?php if (!empty($customer['photo'])): ?>
+        <img src="<?php echo $customer['photo']; ?>" alt="Foto Atual" style="max-width: 200px; max-height: 200px;"><br>
+		<?php else: ?>
+			<p>Foto não disponível.</p>
+		<?php endif; ?>
+	</dd>
+</dl>
+
+
+
+<div id="actions" class="row mt-2">
+	<div class="col-md-12">
+		<a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-dark">Editar</a>
+		<a href="index.php" class="btn btn-light">Voltar</a>
+	</div>
 </div>
 
-<?php clear_messages(); ?>
 <?php include(FOOTER_TEMPLATE); ?>
